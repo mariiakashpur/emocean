3b184fff973f897a78d94a9cbd8c4bf3a3de5531
mariiakashpur <mariia.kashpur@gmail.com>
2017-11-29 23:19:30 +0100

    initial commit

--- main.py
+++ main.py
@@ -10,8 +10,7 @@ import sys
 
 # Define feature file and train/val/test sets
 data_file = sys.argv[1]
-df = pd.read_csv(data_file)
-df = df.drop(['ID'], axis=1)
+df = pd.read_csv(data_file).drop(['ID'], axis=1)
 encoder = LabelEncoder()
 df['label'] = encoder.fit_transform(df['label'])
 # print df.head(10)
@@ -21,16 +20,14 @@ df['label'] = encoder.fit_transform(df['label'])
 def get_bow(X_train, X_test):
     # TfidfVectorizer=CountVectorizer+TfidfTransformer; stop_words="english" as another optional argument
     tfidf_vect = TfidfVectorizer(binary=True, ngram_range=(1, 2))
-    X_train_sp = sp.sparse.hstack((tfidf_vect.fit_transform(X_train['text']), X_train.drop(['text'], axis=1).values), format='csr')
-    X_test_sp = sp.sparse.hstack((tfidf_vect.transform(X_test['text']), X_test.drop(['text'], axis=1).values), format='csr')
-    return X_train_sp, X_test_sp
+    X_train_sparse = sp.sparse.hstack((tfidf_vect.fit_transform(X_train['text']), X_train.drop(['text'], axis=1).values), format='csr')
+    X_test_sparse = sp.sparse.hstack((tfidf_vect.transform(X_test['text']), X_test.drop(['text'], axis=1).values), format='csr')
+    return X_train_sparse, X_test_sparse
 
 
-X = df.drop(['ID', 'label'], axis=1)
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1)
-X_train_sp, X_test_sp = get_bow(X_train, X_test)
-model = ModelDD(X_train_sp, y_train, X_test_sp, y_test)
-
+X_train, X_test, y_train, y_test = train_test_split(df.drop(['label'], axis=1), df['label'], test_size=0.2, random_state=1)
+X_train_sparse, X_test_sparse = get_bow(X_train, X_test)
+model = ModelDD(X_train_sparse, y_train, X_test_sparse, y_test)
 
 
 print "CROSS-VALIDATION RESULTS\n"
